201f9b12922cc4031fff325c5dc37dad898a5c6b
Anubhav Aaryan <74524090+dalaixlmao@users.noreply.github.com>
2024-10-21 15:13:12 +0530

    Time: 52 ms (27.29%) | Memory: 19.9 MB (27.03%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -2,24 +2,43 @@
 
 class Solution {
 public:
-    int f(int i, vector<int>& a, vector<int> &dp) {
-        if (i >= a.size()-1)
+    int f(int i, vector<int>& a, vector<int>& dp) {
+        if (i >= a.size() - 1)
             return 0;
         if (a[i] == 0)
             return 1e6;
         int mn = 1e6;
-        if(dp[i]!=-1)
-        return dp[i];
+        if (dp[i] != -1)
+            return dp[i];
         for (int j = 1; j <= a[i]; j++) {
             mn = min(mn, 1 + f(i + j, a, dp));
         }
-        return dp[i]=mn;
+        return dp[i] = mn;
     }
 
     int jump(vector<int>& a) {
-        int n=a.size();
-        vector<int> dp(n+1, -1);
-        int x = f(0, a, dp);
-        return x;
+        int n = a.size();
+        vector<int> dp(n + 1, 0);
+
+        for (int i = 0; i < n; i++)
+            if (a[i] == 0)
+                dp[i] = 1e6;
+        dp[n - 1] = 0;
+        dp[n] = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            if (a[i] != 0) {
+                int mn=1e6;
+                for (int j = 1; j <= a[i]; j++) {
+                    if(i==n-1)
+                    mn=min(mn, 0);
+                    else if(i+j>=n-1)
+                    mn=min(mn, 1);
+                    else
+                    mn = min(mn, 1+dp[i+j]);
+                }
+                dp[i]=mn;
+            }
+        }
+        return dp[0];
     }
 };
