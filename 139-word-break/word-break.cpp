5e98fd7f278493fa06cae22e43a0e38e9b687da5
Anubhav Aaryan <74524090+dalaixlmao@users.noreply.github.com>
2025-02-18 23:40:00 +0530

    Time: 17 ms (24.82%) | Memory: 18.9 MB (17.29%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -27,11 +27,41 @@ public:
         return dp[l][r] = p | q;
     }
 
+    string getSub(string& s, int l, int r) { return s.substr(l, r - l + 1); }
+
     bool wordBreak(string s, vector<string>& wordDict) {
         int n = wordDict.size();
         unordered_map<string, bool> mp;
         vector<vector<int>> dp(s.size() + 1, vector<int>(s.size() + 1, -1));
-        for(int i = 0;i<n;i++) mp[wordDict[i]]=true;
-        return f(0, 0, s, mp, dp);
+        for (int i = 0; i < n; i++)
+            mp[wordDict[i]] = true;
+            n = s.size();
+        for (int i = 0; i <= n; i++) {
+            dp[i][n] = 0;
+            if (i < n) {
+                string t = getSub(s, i, n - 1);
+                if (mp.count(t))
+                    dp[i][n - 1] = 1;
+                else
+                    dp[i][n - 1] = 0;
+            }
+        }
+
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = n-2; j >= i; j--) {
+                bool l, r;
+                string t = getSub(s,  i, j);
+                if (mp.count(t)) {
+                    l = dp[j + 1][j + 1] & 1;
+                    r = dp[i][j + 1];
+                } else {
+                    l = 0;
+                    r = dp[i][j + 1];
+                }
+                dp[i][j] = l | r;
+            }
+        }
+
+        return dp[0][0];
     }
 };
