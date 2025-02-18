7df7e8425033f34377ba5b2cee02451955509350
Anubhav Aaryan <74524090+dalaixlmao@users.noreply.github.com>
2025-02-19 00:40:55 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (34.32%) - LeetSync

---
+++ 1252-break-a-palindrome/break-a-palindrome.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string breakPalindrome(string a) {
+        if (a.size()==1)
+            return "";
+        int n = a.size();
+        int flag = 0;
+        for (int i = 0; i < n / 2; i++) {
+            if (a[i] != 'a') {
+                a[i] = 'a';
+                flag = 1;
+                break;
+            }
+        }
+        if (flag)
+            return a;
+        a[n - 1] = 'b';
+        return a;
+    }
+};
