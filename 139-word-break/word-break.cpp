1945acbddb822f9a00e2ef324e79b023a4d2ead3
Anubhav Aaryan <74524090+dalaixlmao@users.noreply.github.com>
2025-02-18 23:40:11 +0530

    Time: 15 ms (34.67%) | Memory: 19.1 MB (17.03%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -1,32 +1,5 @@
 class Solution {
 public:
-    bool f(int l, int r, string& s, unordered_map<string, bool>& g,
-           vector<vector<int>>& dp) {
-
-        if (r >= s.size())
-            return false;
-
-        if (dp[l][r] != -1)
-            return dp[l][r];
-
-        string t = s.substr(l, r - l + 1);
-
-        if (r == s.size() - 1) {
-            if (g.count(t))
-                return true;
-            return false;
-        }
-        bool p, q;
-        if (g.count(t)) {
-            p = f(r + 1, r + 1, s, g, dp) & 1;
-            q = f(l, r + 1, s, g, dp);
-        } else {
-            p = 0;
-            q = f(l, r + 1, s, g, dp);
-        }
-        return dp[l][r] = p | q;
-    }
-
     string getSub(string& s, int l, int r) { return s.substr(l, r - l + 1); }
 
     bool wordBreak(string s, vector<string>& wordDict) {
