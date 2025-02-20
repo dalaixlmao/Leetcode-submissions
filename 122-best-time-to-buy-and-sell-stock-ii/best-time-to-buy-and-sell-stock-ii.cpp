ad150315508ed3d728c6035ffe7d4126fc0553e5
Anubhav Aaryan <74524090+dalaixlmao@users.noreply.github.com>
2025-02-21 00:30:53 +0530

    Time: 6 ms (18.60%) | Memory: 22.1 MB (12.80%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
+    int f(int i, int buy, vector<int>& p, vector<vector<int>>& dp) {
+
+        if (i >= p.size())
+            return 0;
+        if (dp[i][buy] != -1)
+            return dp[i][buy];
+        int mx = 0;
+        if (buy) {
+            mx = max(f(i + 1, 0, p, dp) - p[i], f(i + 1, 1, p, dp));
+        } else {
+            mx = max(f(i + 1, 1, p, dp) + p[i], f(i + 1, 0, p, dp));
+        }
+        return dp[i][buy] = mx;
+    }
+
     int maxProfit(vector<int>& p) {
         int n = p.size();
-        // vector<vector<int>> dp(n + 1, vector<int>(2, 0));
-        vector<int> curr(2, 0);
-        vector<int> prv(2, 0);
-        for (int i = n - 1; i >= 0; i--) {
-            for (int j = 0; j < 2; j++) {
-                if (j == 0) {
-                    curr[j] = max(prv[1] - p[i], prv[0]);
-                } else {
-                    curr[j] = max(prv[0] + p[i], prv[1]);
-                }
-                prv = curr;
-            }
-        }
-        return curr[0];
+        vector<vector<int>> dp(n + 1, vector<int>(3, -1));
+        return f(0, 1, p, dp);
     }
 };
